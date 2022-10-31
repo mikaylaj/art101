f40edc53f1f5e2940aaff788a5160082f0869527
mikaylaj <mmjordan@ucsc.edu>
2022-10-31 10:11:56 -0700

    update

---
+++ lab9/js/lab.js
@@ -0,0 +1,10 @@
+/**
+ * Author: Mikayla Jordan & Jaq Moore
+ * Created: Oct. 29 2022
+ *
+ * Art 101
+ **/
+var outputEL = lab9.getElementByID("output");
+var newEl1 = lab9.createElement("P");
+var newEL2 = lab9.createElement
+newEl1.innerHTML = "something"
